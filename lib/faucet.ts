4f959561024cf815e65880c8641af9c960ab8c0e
Alberto García Planes <alberto@paella.dev>
2025-04-14 12:58:29 +0200

    disableRetryOnRateLimit

--- lib/faucet.ts
+++ lib/faucet.ts
@@ -2,7 +2,10 @@ import { Connection, LAMPORTS_PER_SOL, type PublicKey } from "@solana/web3.js";
 
 export async function airdropUsingConnection(walletPubKey: PublicKey) {
   const connection = new Connection(
-    process.env.NEXT_PUBLIC_RPC_URL || "https://api.devnet.solana.com"
+    process.env.NEXT_PUBLIC_RPC_URL || "https://api.devnet.solana.com",
+    {
+      disableRetryOnRateLimit: true,
+    }
   );
   await connection.requestAirdrop(walletPubKey, LAMPORTS_PER_SOL);
 }
